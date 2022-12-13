89062b146be28ed47476138f686060de4118ead2
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-13 13:27:55 +0200

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
 import React from "react";
+import user from "../data/user";
 
-function About() {
+function Links (props){
+  return (<div>
+    <h3>Links</h3>
+    <a href={props.github}>{props.github}</a>
+    <a href={props.linkedin}>{props.linkedin}</a>
+  </div>)
+};
+
+function BioRender ( {isAString} ) {
+
+  return (<div>
+            <div>{isAString ? <p> {user.bio} </p> : null }</div>
+         </div>)
+
+} ;
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
+      <BioRender isAString={true}/>
     </div>
   );
 }
+
 
 export default About;
